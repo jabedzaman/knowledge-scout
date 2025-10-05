006aa53211438530081d5829bec714557833bd0d
jabedzaman <hi@jabed.dev>
2025-10-05 11:54:21 +0530

    feat: connected to mongoose from api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,13 @@
 import { app } from "./app";
 import { serve } from "@hono/node-server";
 import { env } from "./config";
+import { connectToMongo } from "./lib";
 
 /**
  * The main entry point for the application.
  */
-const main = () => {
+const main = async () => {
+  await connectToMongo();
   serve({ fetch: app.fetch, port: env.HTTP_PORT });
   console.log(`Server is running on http://localhost:${env.HTTP_PORT}`);
 };
